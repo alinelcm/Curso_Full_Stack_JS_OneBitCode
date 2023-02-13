86459a8f89f79aec95568e24589b9e52aff5298e
alinelcm <121167604+alinelcm@users.noreply.github.com>
2023-02-13 14:05:00 -0300

    ex 12

--- 5-JavaScript2/ex12.js
+++ 5-JavaScript2/ex12.js
@@ -1,16 +1,104 @@
-const form = document.getElementById("registerForm");
+function createInput(id, value, name, type = "text") {
+  const input = document.createElement("input");
+  input.id = id;
+  input.value = value;
+  input.name = name;
+  input.type = type;
+  return input;
+}
 
-//form.addEventListener("submit", function (ev) {});
+function createLabel(text, htmlFor) {
+  const label = document.createElement("label");
+  label.htmlFor = htmlFor;
+  label.innerText = text;
+  return label;
+}
 
 const addTechBtn = document.getElementById("addTechBtn");
+const form = document.getElementById("registerForm");
+const developers = [];
+let inputRows = 0;
 
 addTechBtn.addEventListener("click", function (ev) {
-  ev.preventDefault();
-  const newTechDiv = document.getElementById("newTechDiv");
-  const newTechInput = document.createElement("input");
-  newTechInput.type = "text";
-  newTechInput.id = "newTech";
-  newTechInput.innerHTML = '<label for="newTech">Tecnologia:</label>';
+  const stackList = document.getElementById("stackList");
 
-  newTechDiv.appendChild(newTechInput);
+  const newRow = document.createElement("li");
+  const rowIndex = inputRows;
+  inputRows++;
+  newRow.id = "inputRow-" + rowIndex;
+  newRow.className = "inputRow";
+
+  const techLabel = createLabel("Nome: ", "techName-" + rowIndex);
+  const techInput = createInput("techName-" + rowIndex, null, "techName");
+
+  const experienceLabel = createLabel("  Tempo de experiência: ");
+  const expRadio1 = createInput(
+    "exp" + rowIndex + ".1",
+    "0-2 anos",
+    "experience" + rowIndex,
+    "radio"
+  );
+  const expRadio1Label = createLabel("0-2 Anos ", "experience" + rowIndex);
+  const expRadio2 = createInput(
+    "exp" + rowIndex + ".2",
+    "3-4 anos",
+    "experience" + rowIndex,
+    "radio"
+  );
+  const expRadio2Label = createLabel("3-4 Anos ", "experience" + rowIndex);
+  const expRadio3 = createInput(
+    "exp" + rowIndex + ".3",
+    "5+ anos",
+    "experience" + rowIndex,
+    "radio"
+  );
+  const expRadio3Label = createLabel("5+ Anos ", "experience" + rowIndex);
+
+  const removeRow = document.createElement("button");
+  removeRow.type = "button";
+  removeRow.innerText = "Remover";
+  removeRow.addEventListener("click", function () {
+    stackList.removeChild(newRow);
+  });
+
+  stackList.appendChild(newRow);
+  newRow.append(
+    techLabel,
+    techInput,
+    experienceLabel,
+    expRadio1,
+    expRadio1Label,
+    expRadio2,
+    expRadio2Label,
+    expRadio3,
+    expRadio3Label,
+    removeRow
+  );
+});
+
+form.addEventListener("submit", function (ev) {
+  ev.preventDefault();
+  const fullName = document.getElementById("name");
+  const rows = document.querySelectorAll(".inputRow");
+
+  let techs = [];
+  rows.forEach(function (row) {
+    const tech = document.querySelector(
+      "#" + row.id + ' input[name="techName"]'
+    ).value;
+    const experience = document.querySelector(
+      "#" + row.id + ' input[type="radio"]:checked'
+    ).value;
+    techs.push({ name: tech, exp: experience });
+  });
+
+  const newDev = { fullName: fullName.value, techs: techs };
+  developers.push(newDev);
+  alert("Dev Cadastrado com sucesso");
+
+  fullName.value = "";
+  rows.forEach(function (row) {
+    row.remove();
+  });
+  console.log(developers);
 });
