459f194f03a13028a9229f9d0fbdc190fd655832
alinelcm <121167604+alinelcm@users.noreply.github.com>
2023-02-15 15:14:51 -0300

    att
    +copy to clipboard fucntion
    +change thme

--- 5-JavaScript2/Projetos Praticos/Calculadora/index.js
+++ 5-JavaScript2/Projetos Praticos/Calculadora/index.js
@@ -57,6 +57,41 @@ textInput.addEventListener("keydown", function (ev) {
 });
 
 function calculate() {
+  resultsInput.value = "Error!";
+  resultsInput.classList.add("error");
   const result = eval(textInput.value);
   resultsInput.value = result;
+  resultsInput.classList.remove("error");
 }
+
+const main = document.querySelector("main");
+const root = document.querySelector(":root");
+
+document.getElementById("themeSwitcher").addEventListener("click", function () {
+  if (main.dataset.theme === "dark") {
+    root.style.setProperty("--background-color", "#cfdce7");
+    root.style.setProperty("--border-color", "#000911");
+    root.style.setProperty("--primary-color", "#07657c");
+    root.style.setProperty("--font-color", "#000911");
+    main.dataset.theme = "light";
+  } else {
+    root.style.setProperty("--background-color", "#000911");
+    root.style.setProperty("--border-color", "#556471");
+    root.style.setProperty("--primary-color", "#24a2c2");
+    root.style.setProperty("--font-color", "#cfdce7");
+    main.dataset.theme = "dark";
+  }
+});
+
+document.getElementById("copy").addEventListener("click", function (ev) {
+  const button = ev.currentTarget;
+  if (button.innerText === "Copy") {
+    button.innerText = "Copied!";
+    button.classList.add("copied");
+    navigator.clipboard.writeText(resultsInput.value);
+    resultsInput.value = "";
+  } else {
+    button.innerText = "Copy";
+    button.classList.remove("copied");
+  }
+});
